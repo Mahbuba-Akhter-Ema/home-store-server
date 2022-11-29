a1716eb6ec4c56bfa5a66524f054d3a32ce22f7b
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-29 18:40:57 +0600

    basic setup done

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-
+const port = process.env.PORT || 5000;
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
+// midleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('Home store is running on server')
+});
+
+app.listen(port, () => console.log(`Home store is listening server ${port}`));
