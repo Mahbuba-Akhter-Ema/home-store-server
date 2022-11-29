5e9772547b4f332658b7b34a2f00ee7551358af5
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-29 19:23:03 +0600

    bedroom data added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 
 // midleware 
@@ -10,13 +11,29 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.trknobu.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER};:${process.env.DB_PASSWORD}@cluster0.trknobu.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+// BedRoom Data Collection MongoDB
+async function run (){
+    try{
+        const bedroomCollection = client.db('HomeStore').collection('Bedroom');
+
+        app.get('/bedroom', async(req, res) => {
+            const query = {};
+            const results = await bedroomCollection.find(query).toArray();
+            res.send(results);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log);
+
 
 
 app.get('/', async (req, res) => {
