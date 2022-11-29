708e6f626844668327652fdf5137063a630e19b5
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-29 18:53:58 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 // midleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.trknobu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', async (req, res) => {
     res.send('Home store is running on server')
