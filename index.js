059ef2a12871cd1f04461d20d4fc902558e7433f
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-11-30 19:07:04 +0600

    save user

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ async function run (){
         const Bedroom = client.db('HomeStore').collection('Bedroom');
         const livingRoomCollection = client.db('HomeStore').collection('livingRoom');
         const dinningRoomCollection = client.db('HomeStore').collection('dinningRoom');
+        const addProductCollection = client.db('HomeStore').collection('addProduct')
+        const sellerCollection = client.db('HomeStore').collection('Seller')
+        const buyerCollection = client.db('HomeStore').collection('Buyer')
 
         // bedroom 
         app.get('/bedRoom', async(req, res) => {
@@ -29,13 +32,39 @@ async function run (){
             const bedRoomResults = await Bedroom.find(query).toArray();
             res.send(bedRoomResults);
         })
+
         // livingRoom 
         app.get('/livingRoom', async(req, res) => {
             const query = {};
             const livingRoomResults = await livingRoomCollection.find(query).toArray();
             res.send(livingRoomResults);
         })
-        
+
+         // dinningRoom 
+         app.get('/dinningRoom', async(req, res) => {
+            const query = {};
+            const dinningRoomResults = await dinningRoomCollection.find(query).toArray();
+            res.send(dinningRoomResults);
+        }) 
+
+        app.post('/position', async (req, res) => {
+            const user = req.body;
+
+            if (user.position === "Buyer") {
+                const result = await buyerCollection.insertOne(user)
+                return res.send(result)
+            }
+
+            if (user.position === "Seller") {
+                const result = await sellerCollection.insertOne(user)
+                return res.send(result)
+            }
+
+            const result = await buyerCollection.insertOne(user)
+            res.send(result)
+
+        })
+               
     }
     finally{
 
