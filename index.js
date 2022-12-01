6886ffe2a27f132d6f06425ac582e5e5192fae59
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-12-01 12:58:21 +0600

    added get orcers etc

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 // Data Collection MongoDB
-async function run (){
-    try{
+async function run() {
+    try {
         const Bedroom = client.db('HomeStore').collection('Bedroom');
         const livingRoomCollection = client.db('HomeStore').collection('livingRoom');
         const dinningRoomCollection = client.db('HomeStore').collection('dinningRoom');
@@ -31,42 +31,42 @@ async function run (){
 
 
         // bedroom 
-        app.get('/bedRoom', async(req, res) => {
+        app.get('/bedRoom', async (req, res) => {
             const query = {};
             const bedRoomResults = await Bedroom.find(query).toArray();
             res.send(bedRoomResults);
         })
 
         // livingRoom 
-        app.get('/livingRoom', async(req, res) => {
+        app.get('/livingRoom', async (req, res) => {
             const query = {};
             const livingRoomResults = await livingRoomCollection.find(query).toArray();
             res.send(livingRoomResults);
         })
 
-         // dinningRoom 
-         app.get('/dinningRoom', async(req, res) => {
+        // dinningRoom 
+        app.get('/dinningRoom', async (req, res) => {
             const query = {};
             const dinningRoomResults = await dinningRoomCollection.find(query).toArray();
             res.send(dinningRoomResults);
-        }) 
+        })
 
         // get seller 
-        app.get('/allSeller', async(req, res) => {
+        app.get('/allSeller', async (req, res) => {
             const query = {};
             const result = await sellerCollection.find(query).toArray();
             res.send(result)
         })
 
         // get Buyer 
-        app.get('/allBuyer', async(req, res) => {
+        app.get('/allBuyer', async (req, res) => {
             const query = {};
             const result = await buyerCollection.find(query).toArray();
             res.send(result)
         })
 
         // getReport 
-        app.get('/allReport', async(req, res) => {
+        app.get('/allReport', async (req, res) => {
             const query = {};
             const result = await reportCollection.find(query).toArray();
             res.send(result)
@@ -90,11 +90,44 @@ async function run (){
 
         })
 
+        // add Product
+        app.post('/products', async (req, res) => {
+            const user = req.body;
+            if (user.brand === "BedRoom") {
+                const result = await Bedroom.insertOne(user)
+                return res.send(result)
+            }
+            if (user.brand === "DinningRoom") {
+                const result = await dinningRoomCollection.insertOne(user)
+                return res.send(result)
+            }
+            if (user.brand === "LivingRoom") {
+                const result = await livingRoomCollection.insertOne(user)
+                return res.send(result)
+            }
+        })
         // to post booking  
         app.post('/booking', async (req, res) => {
-            const user=req.body;
+            const user = req.body;
             const booking = await bookingCollection.insertOne(user)
             res.send(booking)
+        })
+
+        app.get('/booking', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const bookingData = await bookingCollection.find(query).toArray();
+            res.send(bookingData);
+        })
+
+        // my orders
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email}
+            const bedRoomData = await Bedroom.find(query).toArray();
+            const dinningRoomData = await dinningRoomCollection.find(query).toArray();
+            const livingRoomData = await livingRoomCollection.find(query).toArray();
+            res.send(bedRoomData&&dinningRoomData&&livingRoomData)
         })
 
         // report 
@@ -103,10 +136,11 @@ async function run (){
             const report = await reportCollection.insertOne(reportData)
             res.send(report)
         })
+
         // Delete seller 
         app.delete('/sellerDelete/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const deleteData = await sellerCollection.deleteOne(query)
             res.send(deleteData)
         })
@@ -114,18 +148,26 @@ async function run (){
         // Delete Buyer
         app.delete('/buyerDelete/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const deleteData = await buyerCollection.deleteOne(query)
             res.send(deleteData)
         })
-               
+
+        // delete Report 
+        app.delete('/allReport/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const deleteData = await reportCollection.deleteOne(query)
+            res.send(deleteData)
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
 
-run().catch(error =>{
+run().catch(error => {
     console.log(error);
 });
 
